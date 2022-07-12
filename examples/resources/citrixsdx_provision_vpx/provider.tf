3ffdda95a333f14e60f5d40f6064c0acc7375a5b
Sumanth Lingappa <sumanth.lingappa@citrix.com>
2022-07-12 10:47:23 +0530

    save work

--- examples/resources/citrixsdx_provision_vpx/provider.tf
+++ examples/resources/citrixsdx_provision_vpx/provider.tf
@@ -6,8 +6,8 @@ terraform {
   }
 }
 provider "citrixsdx" {
-  host       = "https://10.222.74.135" # Optionally use CITRIXSDX_HOST env var
+  host       = "https://10.10.10.10" # Optionally use CITRIXSDX_HOST env var
   username   = "nsroot"                # Optionally use CITRIXSDX_USERNAME env var
-  password   = "Notnsroot250$"         # Optionally use CITRIXSDX_PASSWORD env var
+  password   = "secretpassword"         # Optionally use CITRIXSDX_PASSWORD env var
   ssl_verify = false                   # Optionally use CITRIXSDX_SSL_VERIFY env var
 }
