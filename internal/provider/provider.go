0e93ceeb2a77cd3f6a8ec2ce40eb6f360a9ccaab
Rohit Myali <rohit.myali@citrix.com>
2024-02-19 14:13:08 +0530

    Added ntp_param resource
    Signed-off-by: Rohit Myali <rohit.myali@citrix.com>

--- internal/provider/provider.go
+++ internal/provider/provider.go
@@ -14,6 +14,7 @@ import (
 	"terraform-provider-netscalersdx/internal/ns"
 	"terraform-provider-netscalersdx/internal/ns_device_profile"
 	"terraform-provider-netscalersdx/internal/ns_save_config"
+	"terraform-provider-netscalersdx/internal/ntp_param"
 	"terraform-provider-netscalersdx/internal/ntp_server"
 	"terraform-provider-netscalersdx/internal/radius_server"
 	"terraform-provider-netscalersdx/internal/smtp_server"
@@ -115,6 +116,7 @@ func (p *sdxprovider) Resources(_ context.Context) []func() resource.Resource {
 		radius_server.RadiusServerResource,
 		device_group.DeviceGroupResource,
 		blx_device_profile.BlxDeviceProfileResource,
+		ntp_param.NtpParamResource,
 	}
 }
 func (p *sdxprovider) DataSources(_ context.Context) []func() datasource.DataSource {
