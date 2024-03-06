eb4bbbd52543056c779395f99c73e3d77b81b300
Rohit Myali <rohit.myali@citrix.com>
2024-03-06 08:40:03 +0000

    Added mpsuser resource

--- internal/provider/provider.go
+++ internal/provider/provider.go
@@ -11,6 +11,7 @@ import (
 	"terraform-provider-netscalersdx/internal/device_group"
 	"terraform-provider-netscalersdx/internal/device_profile"
 	"terraform-provider-netscalersdx/internal/ldap_server"
+	"terraform-provider-netscalersdx/internal/mpsuser"
 	"terraform-provider-netscalersdx/internal/ns"
 	"terraform-provider-netscalersdx/internal/ns_device_profile"
 	"terraform-provider-netscalersdx/internal/ns_save_config"
@@ -121,6 +122,7 @@ func (p *sdxprovider) Resources(_ context.Context) []func() resource.Resource {
 		snmp_user.SnmpUserResource,
 		syslog_params.SyslogParamsResource,
 		ntp_param.NtpParamResource,
+		mpsuser.MpsuserResource,
 	}
 }
 func (p *sdxprovider) DataSources(_ context.Context) []func() datasource.DataSource {
