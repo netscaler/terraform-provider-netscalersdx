5f5abd3641d9b56132cc1018a89fef340c35393d
Rohit Myali <rohit.myali@citrix.com>
2024-05-21 14:24:15 +0530

    Added ntp_sync resource
    Signed-off-by: Rohit Myali <rohit.myali@citrix.com>

--- internal/provider/provider.go
+++ internal/provider/provider.go
@@ -22,6 +22,7 @@ import (
 	"terraform-provider-netscalersdx/internal/ns_save_config"
 	"terraform-provider-netscalersdx/internal/ntp_param"
 	"terraform-provider-netscalersdx/internal/ntp_server"
+	"terraform-provider-netscalersdx/internal/ntp_sync"
 	"terraform-provider-netscalersdx/internal/radius_server"
 	"terraform-provider-netscalersdx/internal/smtp_server"
 	"terraform-provider-netscalersdx/internal/snmp_alarm_config"
@@ -143,6 +144,7 @@ func (p *sdxprovider) Resources(_ context.Context) []func() resource.Resource {
 		aclrule.AclruleResource,
 		aaa_server.AaaServerResource,
 		snmp_mib.SnmpMibResource,
+		ntp_sync.NtpSyncResource,
 	}
 }
 func (p *sdxprovider) DataSources(_ context.Context) []func() datasource.DataSource {
