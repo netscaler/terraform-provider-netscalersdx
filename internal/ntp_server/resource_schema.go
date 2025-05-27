d5713aaa108358edb4b9eeb6aa8bf903ae88a753
rohit-myali <rohit.myali@citrix.com>
2025-05-27 08:39:27 +0000

    Supported Terraform Import for ntp_server resource
    Signed-off-by: rohit-myali <rohit.myali@citrix.com>

--- internal/ntp_server/resource_schema.go
+++ internal/ntp_server/resource_schema.go
@@ -4,6 +4,8 @@ package ntp_server
 
 import (
 	"context"
+	"terraform-provider-netscalersdx/internal/utils"
+
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/planmodifier"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/stringplanmodifier"
@@ -25,6 +27,7 @@ func ntpServerResourceSchema(ctx context.Context) schema.Schema {
 			},
 			"autokey": schema.BoolAttribute{
 				Optional:            true,
+				Computed:            true,
 				Description:         "Autokey Public Key Authentication.",
 				MarkdownDescription: "Autokey Public Key Authentication.",
 			},
@@ -35,21 +38,25 @@ func ntpServerResourceSchema(ctx context.Context) schema.Schema {
 			},
 			"key_id": schema.Int64Attribute{
 				Optional:            true,
+				Computed:            true,
 				Description:         "Key Identifier for Symmetric Key Authentication.",
 				MarkdownDescription: "Key Identifier for Symmetric Key Authentication.",
 			},
 			"maxpoll": schema.Int64Attribute{
 				Optional:            true,
+				Computed:            true,
 				Description:         "Maximum Poll Interval.",
 				MarkdownDescription: "Maximum Poll Interval.",
 			},
 			"minpoll": schema.Int64Attribute{
 				Optional:            true,
+				Computed:            true,
 				Description:         "Minimum Poll Interval.",
 				MarkdownDescription: "Minimum Poll Interval.",
 			},
 			"preferred_server": schema.BoolAttribute{
 				Optional:            true,
+				Computed:            true,
 				Description:         "NTP Server Preferred.",
 				MarkdownDescription: "NTP Server Preferred.",
 			},
@@ -72,26 +79,51 @@ type ntpServerModel struct {
 	Id              types.String `tfsdk:"id"`
 }
 
+func ntpServerSetAttrFromGet(ctx context.Context, data *ntpServerModel, getResponseData map[string]interface{}) *ntpServerModel {
+
+	data.Autokey = types.BoolValue(utils.StringToBool(getResponseData["autokey"].(string)))
+	data.KeyId = types.Int64Value(utils.StringToInt(getResponseData["key_id"].(string)))
+	data.Maxpoll = types.Int64Value(utils.StringToInt(getResponseData["maxpoll"].(string)))
+	data.Minpoll = types.Int64Value(utils.StringToInt(getResponseData["minpoll"].(string)))
+	data.PreferredServer = types.BoolValue(utils.StringToBool(getResponseData["preferred_server"].(string)))
+	data.Server = types.StringValue(getResponseData["server"].(string))
+
+	return data
+
+}
+
 func ntpServerGetThePayloadFromtheConfig(ctx context.Context, data *ntpServerModel) ntpServerReq {
 	tflog.Debug(ctx, "In ntpServerGetThePayloadFromtheConfig Function")
 	ntpServerReqPayload := ntpServerReq{
-		Autokey:         data.Autokey.ValueBool(),
-		Client:          data.Client.ValueString(),
-		KeyId:           data.KeyId.ValueInt64(),
-		Maxpoll:         data.Maxpoll.ValueInt64(),
-		Minpoll:         data.Minpoll.ValueInt64(),
-		PreferredServer: data.PreferredServer.ValueBool(),
-		Server:          data.Server.ValueString(),
+		Client: data.Client.ValueString(),
+		Server: data.Server.ValueString(),
 	}
+
+	if !data.Autokey.IsNull() && !data.Autokey.IsUnknown() {
+		ntpServerReqPayload.Autokey = data.Autokey.ValueBoolPointer()
+	}
+	if !data.KeyId.IsNull() && !data.KeyId.IsUnknown() {
+		ntpServerReqPayload.KeyId = data.KeyId.ValueInt64Pointer()
+	}
+	if !data.Maxpoll.IsNull() && !data.Maxpoll.IsUnknown() {
+		ntpServerReqPayload.Maxpoll = data.Maxpoll.ValueInt64Pointer()
+	}
+	if !data.Minpoll.IsNull() && !data.Minpoll.IsUnknown() {
+		ntpServerReqPayload.Minpoll = data.Minpoll.ValueInt64Pointer()
+	}
+	if !data.PreferredServer.IsNull() && !data.PreferredServer.IsUnknown() {
+		ntpServerReqPayload.PreferredServer = data.PreferredServer.ValueBoolPointer()
+	}
+
 	return ntpServerReqPayload
 }
 
 type ntpServerReq struct {
-	Autokey         bool   `json:"autokey"`
+	Autokey         *bool  `json:"autokey"`
 	Client          string `json:"client,omitempty"`
-	KeyId           int64  `json:"key_id,omitempty"`
-	Maxpoll         int64  `json:"maxpoll,omitempty"`
-	Minpoll         int64  `json:"minpoll,omitempty"`
-	PreferredServer bool   `json:"preferred_server,omitempty"`
+	KeyId           *int64 `json:"key_id,omitempty"`
+	Maxpoll         *int64 `json:"maxpoll,omitempty"`
+	Minpoll         *int64 `json:"minpoll,omitempty"`
+	PreferredServer *bool  `json:"preferred_server,omitempty"`
 	Server          string `json:"server,omitempty"`
 }
