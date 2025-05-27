440fde1e24c5d0051990de88670d8f24b5a80239
rohit-myali <rohit.myali@citrix.com>
2025-05-27 18:21:24 +0000

    Supported Terraform import for mps resource
    Signed-off-by: rohit-myali <rohit.myali@citrix.com>

--- internal/mps/resource_schema.go
+++ internal/mps/resource_schema.go
@@ -2,7 +2,7 @@ package mps
 
 import (
 	"context"
-	"strconv"
+	"terraform-provider-netscalersdx/internal/utils"
 
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema"
 	"github.com/hashicorp/terraform-plugin-framework/types"
@@ -15,61 +15,73 @@ func mpsResourceSchema() schema.Schema {
 		Attributes: map[string]schema.Attribute{
 			"config_motd": schema.BoolAttribute{
 				Optional:            true,
+				Computed:            true,
 				Description:         "Configure Message of the Day (contents of motd file), this needs to be set true if user wants to configure message if the day.",
 				MarkdownDescription: "Configure Message of the Day (contents of motd file), this needs to be set true if user wants to configure message if the day.",
 			},
 			"deployment_type": schema.StringAttribute{
 				Optional:            true,
+				Computed:            true,
 				Description:         "Indicates the type of deployment of NetScaler ADM: standalone or ha or scaleout..",
 				MarkdownDescription: "Indicates the type of deployment of NetScaler ADM: standalone or ha or scaleout..",
 			},
 			"hist_mig_inprog": schema.BoolAttribute{
 				Optional:            true,
+				Computed:            true,
 				Description:         "Indicates whether the historical tables database migration is in progress or not..",
 				MarkdownDescription: "Indicates whether the historical tables database migration is in progress or not..",
 			},
 			"is_cloud": schema.BoolAttribute{
 				Optional:            true,
+				Computed:            true,
 				Description:         "True if its a cloud deployment.",
 				MarkdownDescription: "True if its a cloud deployment.",
 			},
 			"is_container": schema.BoolAttribute{
 				Optional:            true,
+				Computed:            true,
 				Description:         "True if its a container deployment.",
 				MarkdownDescription: "True if its a container deployment.",
 			},
 			"is_member_of_default_group": schema.BoolAttribute{
 				Optional:            true,
+				Computed:            true,
 				Description:         "Is Member Of Default Group.",
 				MarkdownDescription: "Is Member Of Default Group.",
 			},
 			"is_passive": schema.BoolAttribute{
 				Optional:            true,
+				Computed:            true,
 				Description:         "Indicates the node's state: ACTIVE or PASSIVE in a HA deployment..",
 				MarkdownDescription: "Indicates the node's state: ACTIVE or PASSIVE in a HA deployment..",
 			},
 			"is_thirdparty_vm_supported": schema.BoolAttribute{
 				Optional:            true,
+				Computed:            true,
 				Description:         "True, if third party VM is supported.",
 				MarkdownDescription: "True, if third party VM is supported.",
 			},
 			"maps_apikey": schema.StringAttribute{
 				Optional:            true,
+				Computed:            true,
 				Description:         "Maps API Key.",
 				MarkdownDescription: "Maps API Key.",
 			},
 			"motd": schema.StringAttribute{
 				Optional:            true,
+				Computed:            true,
 				Description:         "Message of the Day (contents of motd file) that can be shown on UI after successful login. Maximum length =  255",
 				MarkdownDescription: "Message of the Day (contents of motd file) that can be shown on UI after successful login. Maximum length =  255",
 			},
 			"privilege_feature": schema.StringAttribute{
 				Optional:            true,
+				Computed:            true,
 				Description:         "privilege_feature.",
 				MarkdownDescription: "privilege_feature.",
 			},
 			"upgrade_agent_version": schema.StringAttribute{
 				Optional:            true,
+				Computed:            true,
 				Description:         "Indicates the next version the agent needs to upgrade to..",
 				MarkdownDescription: "Indicates the next version the agent needs to upgrade to..",
 			},
@@ -100,66 +112,53 @@ type mpsModel struct {
 func mpsGetThePayloadFromtheConfig(ctx context.Context, data *mpsModel) mpsReq {
 	tflog.Debug(ctx, "In mpsGetThePayloadFromtheConfig Function")
 	mpsReqPayload := mpsReq{
-		ConfigMotd:              data.ConfigMotd.ValueBoolPointer(),
-		DeploymentType:          data.DeploymentType.ValueString(),
-		HistMigInprog:           data.HistMigInprog.ValueBoolPointer(),
-		IsCloud:                 data.IsCloud.ValueBoolPointer(),
-		IsContainer:             data.IsContainer.ValueBoolPointer(),
-		IsMemberOfDefaultGroup:  data.IsMemberOfDefaultGroup.ValueBoolPointer(),
-		IsPassive:               data.IsPassive.ValueBoolPointer(),
-		IsThirdpartyVmSupported: data.IsThirdpartyVmSupported.ValueBoolPointer(),
-		MapsApikey:              data.MapsApikey.ValueString(),
-		Motd:                    data.Motd.ValueString(),
-		PrivilegeFeature:        data.PrivilegeFeature.ValueString(),
-		UpgradeAgentVersion:     data.UpgradeAgentVersion.ValueString(),
+		DeploymentType:      data.DeploymentType.ValueString(),
+		MapsApikey:          data.MapsApikey.ValueString(),
+		Motd:                data.Motd.ValueString(),
+		PrivilegeFeature:    data.PrivilegeFeature.ValueString(),
+		UpgradeAgentVersion: data.UpgradeAgentVersion.ValueString(),
 	}
+
+	if !data.ConfigMotd.IsNull() && !data.ConfigMotd.IsUnknown() {
+		mpsReqPayload.ConfigMotd = data.ConfigMotd.ValueBoolPointer()
+	}
+	if !data.HistMigInprog.IsNull() && !data.HistMigInprog.IsUnknown() {
+		mpsReqPayload.HistMigInprog = data.HistMigInprog.ValueBoolPointer()
+	}
+	if !data.IsCloud.IsNull() && !data.IsCloud.IsUnknown() {
+		mpsReqPayload.IsCloud = data.IsCloud.ValueBoolPointer()
+	}
+	if !data.IsContainer.IsNull() && !data.IsContainer.IsUnknown() {
+		mpsReqPayload.IsContainer = data.IsContainer.ValueBoolPointer()
+	}
+	if !data.IsMemberOfDefaultGroup.IsNull() && !data.IsMemberOfDefaultGroup.IsUnknown() {
+		mpsReqPayload.IsMemberOfDefaultGroup = data.IsMemberOfDefaultGroup.ValueBoolPointer()
+	}
+	if !data.IsPassive.IsNull() && !data.IsPassive.IsUnknown() {
+		mpsReqPayload.IsPassive = data.IsPassive.ValueBoolPointer()
+	}
+	if !data.IsThirdpartyVmSupported.IsNull() && !data.IsThirdpartyVmSupported.IsUnknown() {
+		mpsReqPayload.IsThirdpartyVmSupported = data.IsThirdpartyVmSupported.ValueBoolPointer()
+	}
+
 	return mpsReqPayload
 }
 func mpsSetAttrFromGet(ctx context.Context, data *mpsModel, getResponseData map[string]interface{}) *mpsModel {
 	tflog.Debug(ctx, "In mpsSetAttrFromGet Function")
-	if !data.ConfigMotd.IsNull() {
-		val, _ := strconv.ParseBool(getResponseData["config_motd"].(string))
-		data.ConfigMotd = types.BoolValue(val)
-	}
-	if !data.DeploymentType.IsNull() {
-		data.DeploymentType = types.StringValue(getResponseData["deployment_type"].(string))
-	}
-	if !data.HistMigInprog.IsNull() {
-		val, _ := strconv.ParseBool(getResponseData["hist_mig_inprog"].(string))
-		data.HistMigInprog = types.BoolValue(val)
-	}
-	if !data.IsCloud.IsNull() {
-		val, _ := strconv.ParseBool(getResponseData["is_cloud"].(string))
-		data.IsCloud = types.BoolValue(val)
-	}
-	if !data.IsContainer.IsNull() {
-		val, _ := strconv.ParseBool(getResponseData["is_container"].(string))
-		data.IsContainer = types.BoolValue(val)
-	}
-	if !data.IsMemberOfDefaultGroup.IsNull() {
-		val, _ := strconv.ParseBool(getResponseData["is_member_of_default_group"].(string))
-		data.IsMemberOfDefaultGroup = types.BoolValue(val)
-	}
-	if !data.IsPassive.IsNull() {
-		val, _ := strconv.ParseBool(getResponseData["is_passive"].(string))
-		data.IsPassive = types.BoolValue(val)
-	}
-	if !data.IsThirdpartyVmSupported.IsNull() {
-		val, _ := strconv.ParseBool(getResponseData["is_thirdparty_vm_supported"].(string))
-		data.IsThirdpartyVmSupported = types.BoolValue(val)
-	}
-	if !data.MapsApikey.IsNull() {
-		data.MapsApikey = types.StringValue(getResponseData["maps_apikey"].(string))
-	}
-	if !data.Motd.IsNull() {
-		data.Motd = types.StringValue(getResponseData["motd"].(string))
-	}
-	if !data.PrivilegeFeature.IsNull() {
-		data.PrivilegeFeature = types.StringValue(getResponseData["privilege_feature"].(string))
-	}
-	if !data.UpgradeAgentVersion.IsNull() {
-		data.UpgradeAgentVersion = types.StringValue(getResponseData["upgrade_agent_version"].(string))
-	}
+
+	data.ConfigMotd = types.BoolValue(utils.StringToBool(getResponseData["config_motd"].(string)))
+	data.DeploymentType = types.StringValue(getResponseData["deployment_type"].(string))
+	data.HistMigInprog = types.BoolValue(utils.StringToBool(getResponseData["hist_mig_inprog"].(string)))
+	data.IsCloud = types.BoolValue(utils.StringToBool(getResponseData["is_cloud"].(string)))
+	data.IsContainer = types.BoolValue(utils.StringToBool(getResponseData["is_container"].(string)))
+	data.IsMemberOfDefaultGroup = types.BoolValue(utils.StringToBool(getResponseData["is_member_of_default_group"].(string)))
+	data.IsPassive = types.BoolValue(utils.StringToBool(getResponseData["is_passive"].(string)))
+	data.IsThirdpartyVmSupported = types.BoolValue(utils.StringToBool(getResponseData["is_thirdparty_vm_supported"].(string)))
+	data.MapsApikey = types.StringValue(getResponseData["maps_apikey"].(string))
+	data.Motd = types.StringValue(getResponseData["motd"].(string))
+	data.PrivilegeFeature = types.StringValue(getResponseData["privilege_feature"].(string))
+	data.UpgradeAgentVersion = types.StringValue(getResponseData["upgrade_agent_version"].(string))
+
 	return data
 }
 
